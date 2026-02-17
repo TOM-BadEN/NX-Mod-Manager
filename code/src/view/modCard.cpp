d18d8b3c0eca63a54e9d3651f2cd53bdb2dc3807
TOM (Bad EN) <1808461873@qq.com>
2026-02-18 03:48:46 +0800

    重构资源目录结构：将 Mod 类型图标移至 mod 子目录，游戏默认图标移至 game 子目录
    - modCard.cpp：更新 Mod 类型图标路径为 img/mod/modType/
    - gameCard.xml：更新游戏默认图标路径为 img/game/defaultIcon.jpg
    - resources/img/：删除旧的 modType 目录和 defaultIcon.jpg，资源已迁移至新的子目录结构

--- code/src/view/modCard.cpp
+++ code/src/view/modCard.cpp
@@ -11,7 +11,7 @@ ModCard::ModCard() {
 }
 
 void ModCard::setMod(const std::string& name, const std::string& type, bool installed) {
-    m_icon->setImageFromRes("img/modType/" + type + ".jpg");
+    m_icon->setImageFromRes("img/mod/modType/" + type + ".jpg");
     m_name->setText(name);
     m_type->setText("类型：" + std::string(modTypeText(type)));
     m_status->setText(installed ? "模组已安装" : "模组未安装");
